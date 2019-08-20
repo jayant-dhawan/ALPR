463df2234e633dfcd7799841976cdd5e59e67081
Jayant Dhawan <31645553+jayant-dhawan@users.noreply.github.com>
2019-08-21 00:47:49 +0530

    Update detect_plate.py

--- detect_plate.py
+++ detect_plate.py
@@ -50,20 +50,20 @@ for region in regionprops(label_image):
     region_height = max_row - min_row
     region_width = max_col - min_col
 
-# ensuring that the region identified satisfies the condition of a
-# typical license plate
-if region_height >= min_height and region_height <= max_height \
-        and region_width >= min_width and region_width <= max_width \
-        and region_width > region_height:
-    flag = 1
-    plate_like_objects.append(binary_img[min_row:max_row,
-                              min_col:max_col])
-    plate_objects_cordinates.append((min_row, min_col,
-                                    max_row, max_col))
-    rectBorder = patches.Rectangle((min_col, min_row), max_col - min_col,
-                                   max_row - min_row, edgecolor="red",
-                                   linewidth=2, fill=False)
-    ax1.add_patch(rectBorder)
+    # ensuring that the region identified satisfies the condition of a
+    # typical license plate
+    if region_height >= min_height and region_height <= max_height \
+            and region_width >= min_width and region_width <= max_width \
+            and region_width > region_height:
+        flag = 1
+        plate_like_objects.append(binary_img[min_row:max_row,
+                                  min_col:max_col])
+        plate_objects_cordinates.append((min_row, min_col,
+                                        max_row, max_col))
+        rectBorder = patches.Rectangle((min_col, min_row), max_col - min_col,
+                                       max_row - min_row, edgecolor="red",
+                                       linewidth=2, fill=False)
+        ax1.add_patch(rectBorder)
 
 if (flag == 0):
     min_height, max_height, min_width, max_width = plate_dimensions2
